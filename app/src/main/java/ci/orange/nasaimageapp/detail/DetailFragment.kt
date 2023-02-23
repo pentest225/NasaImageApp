77625a94d78e6838a98b9be378e19dc5d9a270d6
pentest225 <patrick.dagouaga@gmail.om>
2023-02-23 05:36:45 +0000

    configuration du projet : j'ai pas encore fini la correction des bug et le lancement du projet

---
+++ app/src/main/java/ci/orange/nasaimageapp/detail/DetailFragment.kt
@@ -0,0 +1,40 @@
+package com.udacity.asteroidradar.detail
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import ci.orange.nasaimageapp.Asteroid
+import ci.orange.nasaimageapp.R
+import ci.orange.nasaimageapp.databinding.FragmentDetailBinding
+
+
+
+class DetailFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = FragmentDetailBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+
+//        val asteroid = DetailFragmentArgs.fromBundle(arguments).selectedAsteroid
+        val args by com.udacity.asteroidradar.detail.DetailFragmentArgs
+            val asteroid =
+        binding.asteroid = asteroid
+
+        binding.helpButton.setOnClickListener {
+            displayAstronomicalUnitExplanationDialog()
+        }
+
+        return binding.root
+    }
+
+    private fun displayAstronomicalUnitExplanationDialog() {
+        val builder = AlertDialog.Builder(this@DetailFragment.requireContext())
+            .setMessage(getString(R.string.astronomica_unit_explanation))
+            .setPositiveButton(android.R.string.ok, null)
+        builder.create().show()
+    }
+}
